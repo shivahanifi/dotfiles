f1d67adcaa3fee903b8c38bfb208b6c4f63c3a71
Shiva <shiva@pop-os.uni.lux>
2024-11-18 17:25:59 +0100

    symlink modified

--- symlink.sh
+++ symlink.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-export DOTFILES_DIR=~/dotfiles
+export DOTFILES_DIR=/home/shiva/dotfiles
 ln -sf $DOTFILES_DIR/.bashrc ~/.bashrc
 #ln -sf $DOTFILES_DIR/.bash_profile ~/.bash_profile
-ln -sf $DOTFILES_DIR/.bash_aliases ~/.bash_aliases
+#ln -sf $DOTFILES_DIR/.bash_aliases ~/.bash_aliases
 #ln -sf $DOTFILES_DIR/.bash_logout ~/.bash_logout
 #ln -sf $DOTFILES_DIR/.bash_prompt ~/.bash_prompt
 echo "Symlinks created for dotfiles."
