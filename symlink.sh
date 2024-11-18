a9e7f4c30ee013dfb21459f41940eb4d0827e3c2
Shiva <shiva@pop-os.uni.lux>
2024-11-18 16:57:20 +0100

    bash folder created

---
+++ symlink.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+DOTFILES_DIR=~/dotfiles
+ln -sf $DOTFILES_DIR/.bashrc ~/.bashrc
+#ln -sf $DOTFILES_DIR/.bash_profile ~/.bash_profile
+ln -sf $DOTFILES_DIR/.bash_aliases ~/.bash_aliases
+#ln -sf $DOTFILES_DIR/.bash_logout ~/.bash_logout
+#ln -sf $DOTFILES_DIR/.bash_prompt ~/.bash_prompt
+echo "Symlinks created for dotfiles."
+source ~/.bashrc
+echo "Bash configuration reloaded."
